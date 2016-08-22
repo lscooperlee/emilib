4e28b8405653596ab489d401c4117fb07e8259b2
cooper <davidontech@gmail.com>
2016-08-22 22:01:02 +1200

    add new test for emi_msg_send_high_level with return data

--- python/emilib/emilib.py
+++ python/emilib/emilib.py
@@ -5,6 +5,7 @@ import struct
 
 _emilib = ctypes.cdll.LoadLibrary("libemi.so")
 
+
 class EMIError(Exception):
 
     def __init__(cls, name):
@@ -13,13 +14,14 @@ class EMIError(Exception):
     def __str__(cls):
         return cls.__name
 
+
 class sockaddr_in(ctypes.Structure):
     _fields_ = [
-                ("sa_family", ctypes.c_ushort, 16),  # sin_family
-                ("sin_port", ctypes.c_ushort, 16),
-                ("sin_addr", ctypes.c_byte * 4),
-                ("__pad", ctypes.c_char * 8)
-            ] 
+        ("sa_family", ctypes.c_ushort, 16),  # sin_family
+        ("sin_port", ctypes.c_ushort, 16),
+        ("sin_addr", ctypes.c_byte * 4),
+        ("__pad", ctypes.c_char * 8)
+    ]
 
 #    def __init__(self, ipaddr="127.0.0.1", port = 1361):
 #        pass
@@ -28,7 +30,8 @@ class sockaddr_in(ctypes.Structure):
 #        super(sockaddr_in, self).__init__(0, cport, cipaddr, 0)
 
     def __str__(self):
-        addr = ".".join([ str(x) for x in struct.unpack(">bbbb", self.sin_addr)])
+        addr = ".".join([str(x)
+                         for x in struct.unpack(">bbbb", self.sin_addr)])
         port = struct.unpack("H", (struct.pack(">H", self.sin_port)))[0]
         return "{0}:{1}".format(addr, port)
 
@@ -36,34 +39,35 @@ class sockaddr_in(ctypes.Structure):
 class emi_addr(ctypes.Structure):
 
     _fields_ = [
-                ("ipv4", sockaddr_in),
-                ("pid_t", ctypes.c_int),
-                ("id", ctypes.c_uint),
-            ]
+        ("ipv4", sockaddr_in),
+        ("pid_t", ctypes.c_int),
+        ("id", ctypes.c_uint),
+    ]
 
-    def __init__(self, ipaddr="0.0.0.0", port = 1361):
+    def __init__(self, ipaddr="0.0.0.0", port=1361):
         cipaddr = ctypes.c_char_p(ipaddr.encode())
         cport = ctypes.c_uint(port)
         _emilib.emi_fill_addr(ctypes.pointer(self), cipaddr, cport)
 
     def __str__(self):
-        return "emi_addr:{{ addr: {0}, pid: {1}, id: {2} }}".format(str(self.ipv4), str(self.pid_t), str(self.id))
+        return "emi_addr:{{ addr: {0}, pid: {1} }}".format(str(self.ipv4), str(self.pid_t), str(self.id))
+
 
 class emi_msg(ctypes.Structure):
 
-    _fields_=[
-                ("dest_addr", emi_addr),
-                ("src_addr", emi_addr),
-                ("flag", ctypes.c_uint, 32),
-                ("count", ctypes.c_uint, 32),
-                ("size", ctypes.c_uint, 32),
-                ("cmd", ctypes.c_uint, 32),
-                ("msg", ctypes.c_uint, 32),
-            ]
+    _fields_ = [
+        ("dest_addr", emi_addr),
+        ("src_addr", emi_addr),
+        ("flag", ctypes.c_uint, 32),
+        ("count", ctypes.c_uint, 32),
+        ("size", ctypes.c_uint, 32),
+        ("cmd", ctypes.c_uint, 32),
+        ("msg", ctypes.c_uint, 32),
+    ]
 
     def __new__(cls, *args, **kwargs):
         size = len(kwargs.get("data", b''))
-        msg_void = ctypes.create_string_buffer(size+ctypes.sizeof(cls))
+        msg_void = ctypes.create_string_buffer(size + ctypes.sizeof(cls))
         EMITYPE = ctypes.POINTER(emi_msg)
         msg = EMITYPE(msg_void)
         msg.contents.size = size
@@ -72,7 +76,7 @@ class emi_msg(ctypes.Structure):
     def __str__(self):
         return "emi_addr:{{ msg: {0}, cmd: {1} }}".format(str(self.msg), str(self.cmd))
 
-    def __init__(self, msgnum = 0, cmd = 0, data = b'', ipaddr="127.0.0.1", port = 1361, flag = 0):
+    def __init__(self, msgnum=0, cmd=0, data=b'', ipaddr="127.0.0.1", port=1361, flag=0):
         cipaddr = ctypes.c_char_p(ipaddr.encode())
 
         size = len(data)
@@ -84,12 +88,12 @@ class emi_msg(ctypes.Structure):
         cmsgnum = ctypes.c_uint(msgnum)
 
 #        cport = ctypes.c_uint(port)
-        _emilib.emi_fill_msg(ctypes.pointer(self), cipaddr, cdata, ccmd, cmsgnum, cflag)
-
+        _emilib.emi_fill_msg(
+            ctypes.pointer(self), cipaddr, cdata, ccmd, cmsgnum, cflag)
 
     @property
     def data(self):
-        ccharp = ctypes.c_char_p(ctypes.addressof(self)+ctypes.sizeof(self))
+        ccharp = ctypes.c_char_p(ctypes.addressof(self) + ctypes.sizeof(self))
         return ccharp.value[:self.size]
 
 
@@ -100,20 +104,25 @@ class emilib:
     __emilib.emi_init.argtypes = (None)
     __emilib.emi_init.restype = ctypes.c_int
 
-    __emilib.emi_msg_register.argtypes = (ctypes.c_uint, 
-            ctypes.CFUNCTYPE(ctypes.c_int, ctypes.POINTER(emi_msg)))
+    __emilib.emi_msg_register.argtypes = (ctypes.c_uint,
+                                          ctypes.CFUNCTYPE(ctypes.c_int, ctypes.POINTER(emi_msg)))
     __emilib.emi_msg_register.restype = ctypes.c_int
 
     __emilib.emi_msg_send.argtypes = (ctypes.POINTER(emi_msg),)
     __emilib.emi_msg_send.restype = ctypes.c_int
 
-    __emilib.emi_msg_send_highlevel.argtypes = (ctypes.c_char_p, ctypes.c_uint, 
-            ctypes.c_void_p, ctypes.c_size_t, ctypes.c_void_p, 
-            ctypes.c_size_t, ctypes.c_uint, ctypes.c_uint)
+    __emilib.emi_msg_send_highlevel.argtypes = (ctypes.c_char_p, ctypes.c_uint,
+                                                ctypes.c_void_p, ctypes.c_size_t, ctypes.c_void_p,
+                                                ctypes.c_size_t, ctypes.c_uint, ctypes.c_uint)
     __emilib.emi_msg_send_highlevel.restype = ctypes.c_int
 
-    __emilib.emi_fill_addr.argtypes = (ctypes.POINTER(emi_addr), ctypes.c_char_p, ctypes.c_uint)
-    __emilib.emi_fill_addr.restype = ctypes.c_int
+    __emilib.emi_msg_prepare_return_data.argtypes = (
+        ctypes.POINTER(emi_msg), ctypes.c_void_p, ctypes.c_uint)
+    __emilib.emi_msg_prepare_return_data.restype = ctypes.c_int
+    
+
+    EMI_MSG_MODE_BLOCK = 0x00000100
+    EMI_MSG_RET_SUCCEEDED = 0x00010000
 
 
     @classmethod
@@ -149,23 +158,23 @@ class emilib:
         cssize = ctypes.c_size_t(size)
         csdata = (ctypes.c_ubyte * size).from_buffer(bytearray(sbytes))
 
-#        crsize = ctypes.c_int(size)
-#        crdata = (ctypes.c_ubyte * size).from_buffer(sbytes[:])
         crsize = ctypes.c_size_t(size)
         crdata = (ctypes.c_ubyte * size).from_buffer(bytearray(size))
 
-        return cls.__emilib.emi_msg_send_highlevel(cipaddr, cmsgnum, 
-                ctypes.pointer(csdata), cssize, ctypes.pointer(crdata), crsize,
-                ccmd, ctypes.c_uint(0))
+        ret = cls.__emilib.emi_msg_send_highlevel(cipaddr, cmsgnum,
+                                                   ctypes.pointer(csdata), cssize, ctypes.pointer(
+                                                       crdata), crsize,
+                                                   ccmd, ctypes.c_uint(0))
+        return ret, crdata
 
-# 
-#     @classmethod
-#     def emi_msg_prepare_return_data(cls, msg, retbytes):
-#         size = len(retbytes)
-#         rdata = (c_ubyte * size).from_buffer(retbytes)
-#         rsize = c_int(size)
-#         return cls.__emilib.emi_msg_prepare_return_data(msg, byref(rdata),
-#                                                          rsize)
+    @classmethod
+    def emi_msg_prepare_return_data(cls, msg, retbytes):
+        size = len(retbytes)
+        rdata = (ctypes.c_ubyte * size).from_buffer(bytearray(retbytes))
+        rsize = ctypes.c_uint(size)
+        return cls.__emilib.emi_msg_prepare_return_data(ctypes.pointer(msg), ctypes.pointer(rdata),
+                                                    rsize)
+
     @classmethod
     def emi_loop(cls):
         cls.__emilib.emi_loop()
--- test/unit_test.py
+++ test/unit_test.py
@@ -8,6 +8,7 @@ import unittest
 from emi_test import EmiTestor
 from emilib import emilib, emi_msg, emi_addr, sockaddr_in
 
+
 class TestEmiLib(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
@@ -16,7 +17,7 @@ class TestEmiLib(unittest.TestCase):
 
     def setUp(self):
         self.emiTestor.runEmiCore()
-        time.sleep(0.2)
+        time.sleep(.2)
 
     def tearDown(self):
         self.emiTestor.stopEmiCore()
@@ -26,22 +27,42 @@ class TestEmiLib(unittest.TestCase):
         self.assertEqual(str(addr.ipv4), "127.1.1.2:256")
 
     def test_emi_msg(self):
-        msg = emi_msg()
-        msg_data = emi_msg(data=b'1234', cmd=1, msgnum=2, flag=3, ipaddr="10.0.0.3")
+        msg_data = emi_msg(
+            data=b'1234', cmd=1, msgnum=2, flag=3, ipaddr="10.0.0.3")
         self.assertEqual(msg_data.data, b'1234')
         self.assertEqual(msg_data.msg, 2)
         self.assertEqual(msg_data.cmd, 1)
         self.assertEqual(msg_data.size, len(b'1234'))
         self.assertEqual(str(msg_data.dest_addr.ipv4), "10.0.0.3:1361")
 
+    def test_emi_msg_prepare_return_data(self):
+        msg_data = emi_msg(data=b'87654321', cmd=2, msgnum=1,
+                           flag=emilib.EMI_MSG_MODE_BLOCK, ipaddr="127.0.0.3")
+        self.assertEqual(msg_data.data, b'87654321')
+        self.assertEqual(msg_data.flag, emilib.EMI_MSG_MODE_BLOCK)
+        self.assertEqual(msg_data.size, len(b'87654321'))
+        self.assertEqual(str(msg_data.dest_addr.ipv4), "127.0.0.3:1361")
+
+        ret = emilib.emi_msg_prepare_return_data(msg_data, b'abcdef')
+
+        self.assertEqual(ret, 0)
+        self.assertEqual(msg_data.data, b'abcdef')
+        self.assertEqual(
+            msg_data.flag, emilib.EMI_MSG_MODE_BLOCK | emilib.EMI_MSG_RET_SUCCEEDED)
+        self.assertEqual(msg_data.size, len(b'abcdef'))
+        self.assertEqual(str(msg_data.dest_addr.ipv4), "127.0.0.3:1361")
+
     def test_emi_init(self):
-         self.assertEqual(emilib.emi_init(), 0)
+        self.assertEqual(emilib.emi_init(), 0)
 
     def test_emi_msg_register(self):
-        emilib.emi_init()
+        ret = emilib.emi_init()
+        self.assertEqual(ret, 0)
+
         def func(msg):
             return 0
         ret = emilib.emi_msg_register(1, func)
+
         self.assertEqual(ret, 0)
 
 #      def test_emi_msg_send(self):
@@ -49,7 +70,7 @@ class TestEmiLib(unittest.TestCase):
 #              print("emi registered")
 #          ret = emilib.emi_msg_register(2, func)
 #          self.assertEqual(ret, 0)
-#          
+#
 #          msg = emi_msg()
 #          msg.msg = 2
 #          ret = emilib.emi_msg_send(msg)
@@ -58,8 +79,9 @@ class TestEmiLib(unittest.TestCase):
 
     def test_emi_msg_send_highlevel_unblock_nosenddata(self):
         emilib.emi_init()
-        
+
         received = True
+
         def func(msg):
             nonlocal received
             self.assertEqual(received, True)
@@ -70,9 +92,10 @@ class TestEmiLib(unittest.TestCase):
 
         ret = emilib.emi_msg_register(2, func)
         self.assertEqual(ret, 0)
- 
+
         ret = emilib.emi_msg_send_highlevel("127.0.0.1", 2, 1)
-         
+        self.assertEqual(ret[0], 0)
+
         time.sleep(1)
 
         self.assertEqual(received, False)
@@ -81,6 +104,7 @@ class TestEmiLib(unittest.TestCase):
         emilib.emi_init()
 
         received = True
+
         def func(msg):
             nonlocal received
             self.assertEqual(received, True)
@@ -94,11 +118,36 @@ class TestEmiLib(unittest.TestCase):
         self.assertEqual(ret, 0)
 
         ret = emilib.emi_msg_send_highlevel("127.0.0.1", 3, 1, b"11112222")
-        
+        self.assertEqual(ret[0], 0)
+
         time.sleep(1)
 
         self.assertEqual(received, False)
-    
+
+    def test_emi_msg_send_highlevel_block_nosenddata_noretdata(self):
+        emilib.emi_init()
+
+        received = True
+
+        def func(msg):
+            nonlocal received
+            self.assertEqual(received, True)
+            self.assertEqual(msg.contents.msg, 4)
+            self.assertEqual(msg.contents.cmd, 1)
+            self.assertEqual(msg.contents.data, b"11112222")
+            received = False
+            return 0
+
+        ret = emilib.emi_msg_register(4, func)
+        self.assertEqual(ret, 0)
+
+        ret = emilib.emi_msg_send_highlevel("127.0.0.1", 4, 1, b"11112222")
+        self.assertEqual(ret[0], 0)
+
+        time.sleep(1)
+
+        self.assertEqual(received, False)
+
 
 if __name__ == "__main__":
     unittest.main()
