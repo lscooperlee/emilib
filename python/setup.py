809f949f1e3492b04f64e4c44d0eb06270823031
cooper <davidontech@gmail.com>
2016-08-23 22:37:45 +1200

    Add simple setup.py for emilib python

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='emilib',
+      version='0.01',
+      description='emilib',
+      author='Cooper Li',
+      author_email='davidontech@gmail.com',
+      url='',
+      packages=['emilib'],
+     )
